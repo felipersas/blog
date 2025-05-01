40030ee757795c4e9bfb445162eeb5a6019ffe0e
Felipe <felipemarques.computacao@gmail.com>
2025-04-30 21:46:31 -0300

    chore: new github user added

--- src/consts.ts
+++ src/consts.ts
@@ -3,5 +3,5 @@
 
 export const SITE_TITLE = '<Felipersas/>';
 export const SITE_DESCRIPTION = 'Felipersas Dev - Blog pessoal sobre desenvolvimento de software e tecnologia.';
-export const GITHUB_URL = 'https://github.com/felipe-marques-dev';
+export const GITHUB_URL = 'https://github.com/felipersas';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/felipe-marques-a748b9299?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app';
